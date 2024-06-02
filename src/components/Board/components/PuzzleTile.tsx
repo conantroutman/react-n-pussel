b01fb63215882b7653c3b9199f01d5e9d5e16832
John Söderberg <john.co.soderberg@gmail.com>
2024-06-02 23:30:22 +0200

    fix: Responsive tile font size

--- src/components/Board/components/PuzzleTile.tsx
+++ src/components/Board/components/PuzzleTile.tsx
@@ -16,7 +16,7 @@ export const PuzzleTile: React.FC<Props> = ({ value, row, col, onClick }) => {
     <button
       tabIndex={1}
       onClick={handleClick}
-      className="w-full aspect-square bg-blue-300 rounded-md text-1/2 hover:bg-blue-400 hover:scale-105 transition"
+      className="w-full aspect-square bg-blue-300 rounded-md text-1/2 hover:bg-blue-400 hover:scale-105 transition text-3xl max-md:text-xl max-sm:text-lg"
     >
       {value}
     </button>
