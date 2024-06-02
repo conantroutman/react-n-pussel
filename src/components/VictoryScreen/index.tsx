d929165ac9f65b02bdcde38db25aa0d8a15d52b6
John Söderberg <john.co.soderberg@gmail.com>
2024-06-02 22:24:39 +0200

    refactor: Improved victory screen layout

--- src/components/VictoryScreen/index.tsx
+++ src/components/VictoryScreen/index.tsx
@@ -5,8 +5,10 @@ export const VictoryScreen = () => {
   const { clicks } = useBoard();
 
   return (
-    <div className="w-full">
-      <div className="text-center font-bold text-6xl uppercase">Du vann!</div>
+    <div className="w-full flex flex-col gap-8">
+      <div className="text-center font-bold text-7xl max-md:text-4xl uppercase">
+        🎊 Du vann! 🎊
+      </div>
       <div className="text-center">
         Du klarade spelet med <span className="font-semibold">{clicks}</span>{" "}
         drag.
