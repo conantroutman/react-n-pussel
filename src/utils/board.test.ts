418b6eca59bf98480358824d772b81bf1fec15c7
John Söderberg <john.co.soderberg@gmail.com>
2024-06-02 20:30:59 +0200

    test: Updated unit tests

--- src/utils/board.test.ts
+++ src/utils/board.test.ts
@@ -7,6 +7,19 @@ test("isBoardSolved returns true when all the board tiles are in ascending order
   expect(BoardUtils.isPuzzleSolved(board)).toBeTruthy();
 });
 
+test("isBoardSolved returns false when the empty tile is not last", () => {
+  const board = BoardUtils.generateBoard(false);
+
+  // First tile is going to be 0/empty
+  // Move it to the end of the board
+  const emptyTile = board[0].shift();
+  if (emptyTile) {
+    board[board.length - 1].push(emptyTile);
+  }
+
+  expect(BoardUtils.isPuzzleSolved(board)).toBeFalsy();
+});
+
 test("isBoardSolved returns false when all the board tiles not in the correct order", () => {
   const board = BoardUtils.generateBoard();
 
